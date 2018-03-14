f5a6e7e5046c42efb9826b9d83911a920d2c8e07
Julian <magic.onastick@gmail.com>
2018-03-14 21:20:38 +0100

    header design fitted on each device size

--- js/ShrinkHeader.js
+++ js/ShrinkHeader.js
@@ -2,12 +2,12 @@ $(window).scroll(function() {
     //gets the initial vertical position of the scrollbar - if it leaves the position, remove class
     if ($(window).scrollTop()){
       $('nav').addClass('black');
-      $('nav').addClass('navbar-light');
-      $('nav').removeClass('navbar-dark');
+      //$('nav').addClass('navbar-light');
+      //$('nav').removeClass('navbar-dark');
     }
     else{
       $('nav').removeClass('black');
-      $('nav').removeClass('navbar-light');
-      $('nav').addClass('navbar-dark');
+      //$('nav').removeClass('navbar-light');
+      //$('nav').addClass('navbar-dark');
     }
   });
